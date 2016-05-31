905ee73efaef2bb4709b021a80f0652c08831ea7
Alfredo AD <alfredo7891@gmail.com>
2016-05-31 12:11:34 -0500

    Les

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,51 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-battery-status/www/battery.js",
+        "id": "cordova-plugin-battery-status.battery",
+        "clobbers": [
+            "navigator.battery"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "cordova-plugin-network-information.network",
+        "clobbers": [
+            "navigator.connection",
+            "navigator.network.connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "id": "cordova-plugin-network-information.Connection",
+        "clobbers": [
+            "Connection"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeoLocation.js",
+        "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeoLocation",
+        "clobbers": [
+            "backgroundGeoLocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-battery-status": "1.1.2",
+    "cordova-plugin-network-information": "1.2.1",
+    "cordova-plugin-mauron85-background-geolocation": "0.9.6",
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-legacy-whitelist": "1.1.1",
+    "cordova-plugin-device": "1.1.2"
+};
+// BOTTOM OF METADATA
+});
